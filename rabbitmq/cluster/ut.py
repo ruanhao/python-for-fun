0132cdeacfbc0bdf8c67ca267edda4c1d0e1e931
Hao Ruan <haoru@cisco.com>
2019-05-28 13:23:44 +0800

    Update ut.UnitTest.test_promotion_of_unsync_mirror_on_master_failure

--- rabbitmq/cluster/ut.py
+++ rabbitmq/cluster/ut.py
@@ -915,7 +915,7 @@ class UnitTest(unittest.TestCase):
             self.assertEqual(pika_basic_get(channel3, queue1), 'msg')
             run('docker exec rabbit2 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
 
-        with self.subTest("ha-promote-on-shutdown is when-synced"):
+        with self.subTest("ha-promote-on-shutdown is when-synced (default)"):
             self._test_creating_cluster()
             run("""docker exec rabbit1 rabbitmqctl set_policy --priority 0 --apply-to queues pl '.*' '{"ha-mode": "all", "ha-promote-on-shutdown": "when-synced"}'""")
             channel1 = pika_channel(port=RABBIT_1_PORT)
@@ -943,7 +943,7 @@ class UnitTest(unittest.TestCase):
             self.assertIn("NOT_FOUND", raised_exception.exception.reply_text)
 
 
-        with self.subTest("ha-promote-on-shutdown is when-synced and node is uncontrolly shutdown"):
+        with self.subTest("ha-promote-on-shutdown is when-synced (default) and node is uncontrolly shutdown"):
             '''
             An uncontrolled master shutdown (i.e. server or node crash, or network outage)
             will still trigger a promotion of an unsynchronised mirror.
@@ -970,6 +970,62 @@ class UnitTest(unittest.TestCase):
             run(f'docker network disconnect {DOCKER_NETWORK} rabbit1')
             time.sleep(90)
 
+            queue1_info = get_queue_info(queue1, port=RABBIT_2_MANAGEMENT_PORT)
+            self.assertIn(queue1_info['node'], ['rabbit@rabbit2', 'rabbit@rabbit3'])
+            run('docker exec rabbit2 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
+
+
+    def test_promotion_of_unsync_mirror_on_master_failure(self):
+        with self.subTest("ha-promote-on-failure is when-synced"):
+            self._test_creating_cluster()
+            run("""docker exec rabbit1 rabbitmqctl set_policy --priority 0 --apply-to queues pl '.*' '{"ha-mode": "all", "ha-promote-on-failure": "when-synced"}'""")
+            channel1 = pika_channel(port=RABBIT_1_PORT)
+            queue1 = pika_queue_declare(channel1, "queue1", durable=True)
+            run('docker exec rabbit1 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
+            for sname in ['rabbit3', 'rabbit2', 'rabbit1']:
+                run(f'docker exec {sname} rabbitmqctl stop_app')
+
+            run('docker exec rabbit1 rabbitmqctl start_app')
+            channel1 = pika_channel(port=RABBIT_1_PORT)
+            pika_simple_publish(channel1, '', queue1, 'new message')  # mock unsync queues
+
+            for sname in ['rabbit2', 'rabbit3']:
+                run(f'docker exec {sname} rabbitmqctl start_app')
+
+            run('docker exec rabbit1 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
+            time.sleep(5)
+            queue1_info = get_queue_info(queue1)
+            self.assertEqual(queue1_info['synchronised_slave_nodes'], [], queue1_info)
+            run(f'docker network disconnect {DOCKER_NETWORK} rabbit1')  # can not use stop_app here
+            time.sleep(90)
+            channel2 = pika_channel(port=RABBIT_2_PORT)
+            pika_simple_publish(channel2, '', queue1, 'msg')  # Message published, but not routed.
+            with self.assertRaises(Exception) as raised_exception:
+                self.assertIsNone(pika_basic_get(channel2, queue1))
+            self.assertIn("NOT_FOUND", raised_exception.exception.reply_text)
+            run('docker exec rabbit2 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
+
+        with self.subTest("ha-promote-on-failure is always (default)"):
+            self._test_creating_cluster()
+            run("""docker exec rabbit1 rabbitmqctl set_policy --priority 0 --apply-to queues pl '.*' '{"ha-mode": "all", "ha-promote-on-failure": "always"}'""")
+            channel1 = pika_channel(port=RABBIT_1_PORT)
+            queue1 = pika_queue_declare(channel1, "queue1", durable=True)
+            run('docker exec rabbit1 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
+            for sname in ['rabbit3', 'rabbit2', 'rabbit1']:
+                run(f'docker exec {sname} rabbitmqctl stop_app')
+            run('docker exec rabbit1 rabbitmqctl start_app')
+            channel1 = pika_channel(port=RABBIT_1_PORT)
+            pika_simple_publish(channel1, '', queue1, 'new message')  # mock unsync queues
+
+            for sname in ['rabbit2', 'rabbit3']:
+                run(f'docker exec {sname} rabbitmqctl start_app')
+
+            run('docker exec rabbit1 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
+            time.sleep(5)
+            queue1_info = get_queue_info(queue1)
+            self.assertEqual(queue1_info['synchronised_slave_nodes'], [])
+            run(f'docker network disconnect {DOCKER_NETWORK} rabbit1')  # can not use stop_app here
+            time.sleep(90)
             queue1_info = get_queue_info(queue1, port=RABBIT_2_MANAGEMENT_PORT)
             self.assertIn(queue1_info['node'], ['rabbit@rabbit2', 'rabbit@rabbit3'])
             run('docker exec rabbit2 rabbitmqctl list_queues name pid slave_pids synchronised_slave_pids | column -t')
