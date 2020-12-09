01c954fd47c37f5aa79c8631d6d9c1dcd7469466
Hao Ruan <haoru@cisco.com>
2020-12-09 19:09:07 +0800

    Redis cluster manually

---
+++ redis/cluster/manually/_cleanup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Description:
+
+. _common.sh
+
+info "Cleaning up ..."
+for i in {1..3}; do
+    info "Stopping master$i ..."
+    vagrant ssh master$i -- sudo service redis-server stop
+    info "Stopping slave$i ..."
+    vagrant ssh slave$i -- sudo service redis-server stop
+    info "Removing conf for master$i ..."
+    vagrant ssh master$i -- sudo rm -rf /var/redis/*
+    info "Removing conf for slave$i ..."
+    vagrant ssh slave$i -- sudo rm -rf /var/redis/*
+done
+for i in {1..3}; do
+    info "Starting master$i ..."
+    vagrant ssh master$i -- sudo service redis-server start
+    info "Starting slave$i ..."
+    vagrant ssh slave$i -- sudo service redis-server start
+done
+info "Cleaning up over"
--- redis/cluster/manually/manual_steps.sh
+++ redis/cluster/manually/manual_steps.sh
@@ -5,26 +5,31 @@
 
 set -e
 
+. _common.sh
+
+bash _cleanup.sh
+
+
 # STEP 1: handshake
-echo "[Meeting] master1 => master2 ..."
+info "[Meeting] master1 => master2 ..."
 vagrant ssh master1 -- redis-cli cluster meet 192.168.33.12 6379 # master2
-echo "[Meeting] master1 => master3 ..."
+info "[Meeting] master1 => master3 ..."
 vagrant ssh master1 -- redis-cli cluster meet 192.168.33.13 6379 # master3
-echo "[Meeting] master1 => slave1 ..."
+info "[Meeting] master1 => slave1 ..."
 vagrant ssh master1 -- redis-cli cluster meet 192.168.33.21 6379 # master3
-echo "[Meeting] master1 => slave2 ..."
+info "[Meeting] master1 => slave2 ..."
 vagrant ssh master1 -- redis-cli cluster meet 192.168.33.22 6379 # master3
-echo "[Meeting] master1 => slave3 ..."
+info "[Meeting] master1 => slave3 ..."
 vagrant ssh master1 -- redis-cli cluster meet 192.168.33.23 6379 # master3
 
 python3 -m unittest steps_test.UnitTest.test_AFTER_MEET
 
 # STEP 2: allocate slots
-echo "[Adding slots] master1 ..."
+info "[Adding slots] master1 ..."
 vagrant ssh master1 -- redis-cli cluster addslots {0..5461}
-echo "[Adding slots] master2 ..."
+info "[Adding slots] master2 ..."
 vagrant ssh master2 -- redis-cli cluster addslots {5462..10922}
-echo "[Adding slots] master3 ..."
+info "[Adding slots] master3 ..."
 vagrant ssh master3 -- redis-cli cluster addslots {10923..16383}
 sleep 10                         # wait a while
 python3 -m unittest steps_test.UnitTest.test_AFTER_ADDSLOTS
@@ -34,11 +39,11 @@ python3 -m unittest steps_test.UnitTest.test_AFTER_ADDSLOTS
 master1_node_id=$( vagrant ssh master1 -- redis-cli cluster myid )
 master2_node_id=$( vagrant ssh master2 -- redis-cli cluster myid )
 master3_node_id=$( vagrant ssh master3 -- redis-cli cluster myid )
-echo "[Replicate] slave1 -> master1 ($master1_node_id)"
+info "[Replicate] slave1 -> master1 ($master1_node_id)"
 vagrant ssh slave1 -- redis-cli cluster replicate $master1_node_id
-echo "[Replicate] slave2 -> master1 ($master2_node_id)"
+info "[Replicate] slave2 -> master1 ($master2_node_id)"
 vagrant ssh slave2 -- redis-cli cluster replicate $master2_node_id
-echo "[Replicate] slave3 -> master1 ($master3_node_id)"
+info "[Replicate] slave3 -> master1 ($master3_node_id)"
 vagrant ssh slave3 -- redis-cli cluster replicate $master3_node_id
-sleep 3
+sleep 10
 python3 -m unittest steps_test.UnitTest.test_AFTER_REPLICATE
