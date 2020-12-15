dbec45b3453cedeaacf3e1f793981dc1ceecafed
Hao Ruan <haoru@cisco.com>
2020-12-15 20:40:22 +0800

    Update vagrant setup for k8s

--- k8s/k8s_bootstrap/bootstrap.sh
+++ k8s/k8s_bootstrap/bootstrap.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
 
+KUBERNETES_VERSION=1.18.12-00
+
+cat <<EOF | sudo -u vagrant tee /home/vagrant/.inputrc
+"\C-p": history-search-backward
+"\C-n": history-search-forward
+EOF
+
 # Update hosts file
 echo "[TASK 1] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.42.42.100 kmaster.example.com kmaster
-172.42.42.101 kworker1.example.com kworker1
-172.42.42.102 kworker2.example.com kworker2
+172.42.42.100 master
+172.42.42.101 worker1
+172.42.42.102 worker2
 EOF
 
 
-echo "[TASK 2] Install docker container engine"
+echo "[TASK 2] Adding apt repo"
+apt-get update -y
 apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+# Add kubernetes sources list into the sources.list directory
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 apt-get update -y
+
+echo "[TASK 3] Install docker container engine"
 apt-get install docker-ce -y
 
 # add ccount to the docker group
 usermod -aG docker vagrant
 
 # Enable docker service
-echo "[TASK 3] Enable and start docker service"
+echo "[TASK 4] Enable and start docker service"
 systemctl enable docker >/dev/null 2>&1
 systemctl start docker
 
 # Add sysctl settings
-echo "[TASK 6] Add sysctl settings"
+echo "[TASK 5] Add sysctl settings"
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -33,40 +50,29 @@ EOF
 sysctl --system >/dev/null 2>&1
 
 # Disable swap
-echo "[TASK 7] Disable and turn off SWAP"
+echo "[TASK 6] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
-# Install apt-transport-https pkg
-echo "[TASK 8] Installing apt-transport-https pkg"
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-# Add he kubernetes sources list into the sources.list directory
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-ls -ltr /etc/apt/sources.list.d/kubernetes.list
-
-apt-get update -y
-
 # Install Kubernetes
-echo "[TASK 9] Install Kubernetes kubeadm, kubelet and kubectl"
-apt-get install -y kubelet kubeadm kubectl
+echo "[TASK 7] Install Kubernetes kubeadm, kubelet and kubectl"
+apt-get install -y \
+        kubelet=$KUBERNETES_VERSION \
+        kubeadm=$KUBERNETES_VERSION \
+        kubectl=$KUBERNETES_VERSION
 
 # Start and Enable kubelet service
-echo "[TASK 10] Enable and start kubelet service"
+echo "[TASK 8] Enable and start kubelet service"
 systemctl enable kubelet >/dev/null 2>&1
 systemctl start kubelet >/dev/null 2>&1
 
 # Enable ssh password authentication
-echo "[TASK 11] Enable ssh password authentication"
+echo "[TASK 9] Enable ssh password authentication"
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart sshd
 
 # Set Root password
-echo "[TASK 12] Set root password"
+echo "[TASK 10] Set root password"
 echo -e "kubeadmin\nkubeadmin" | passwd root
 #echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
 
---
+++ k8s/k8s_bootstrap/bootstrap_master.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Initialize Kubernetes
+echo "[TASK 1] Initialize Kubernetes Cluster"
+kubeadm init --apiserver-advertise-address=172.42.42.100 --pod-network-cidr=192.168.0.0/16 | tee /root/kubeinit.log
+
+# Copy Kube admin config
+echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
+mkdir /home/vagrant/.kube
+cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown -R vagrant:vagrant /home/vagrant/.kube
+
+# Deploy flannel network
+echo "[TASK 3] Deploy Calico network"
+su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml"
+
+# Generate Cluster join command
+echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+kubeadm token create --print-join-command | tee /joincluster.sh
+
+apt-get install bash-completion -y
+kubectl completion bash > /etc/bash_completion.d/kubectl
+curl -o /etc/kube-ps1.sh https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
+cat <<'EOF' | tee -a /home/vagrant/.bashrc
+alias ll="ls -lhrt"
+alias ..='cd ..'
+alias ...='.2'
+alias ....='.3'
+alias .....='.4'
+alias ......='.5'
+alias .2='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+alias kb=kubectl
+complete -F __start_kubectl kb
+
+source /etc/kube-ps1.sh
+export PS1='[\u@\h \W $(kube_ps1)]\$ '
+EOF
